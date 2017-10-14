a8fc02edf1f77b9e7c8502f99db2134dbda25a04
billscarp <billscarp@gmail.com>
2017-10-14 14:09:48 -0400

    set up mongo tasks are rdering to pagegit add -A

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,10 +5,25 @@ var express = require('express');
 // uses the express router
 var router = express.Router();
 
+// require mongojs
+
+var mongojs = require('mongojs');
+
+// create database object / added mongo driver with my username and password & Object array tasks
+
+var db = mongojs('mongodb://billscarp:bonehead@ds113063.mlab.com:13063/mytasklist_billy', ['tasks'])
+
 // sets router to accept get request from home page
 router.get('/tasks', function(req, res, next){
-    //sends what I want to the api/tasks page
-    res.send('Hello there billy');
+    //searches for the mongo database
+    db.tasks.find(function(err, tasks) {
+        if(err){
+            res.send(err);
+        }
+            res.json(tasks);
+
+
+    });
 });
 
 
