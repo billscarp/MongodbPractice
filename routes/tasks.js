212b714c5e004aefa8e75ddd8105f2955f3b4e31
billscarp <billscarp@gmail.com>
2017-10-14 14:35:57 -0400

    added lot of comments and ability to search for one task by ID

--- routes/tasks.js
+++ routes/tasks.js
@@ -15,16 +15,37 @@ var db = mongojs('mongodb://billscarp:bonehead@ds113063.mlab.com:13063/mytasklis
 
 // sets router to accept get request from home page
 router.get('/tasks', function(req, res, next){
-    //searches for the mongo database
+    //searches for tasks / function looks for tasks in the mongo database or throws err  / gets all tasks
     db.tasks.find(function(err, tasks) {
         if(err){
+            // if err, return error
             res.send(err);
-        }
+        }   // if not, return json file
             res.json(tasks);
 
 
     });
 });
+
+// Get single tasks
+ // changes tasks to task/ Id is placeholder, because it changes/ 
+router.get('/task/:id', function(req, res, next){
+// use findOne to find one object.  Used Object ID to look at mongojs
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+        if(err){
+            // if err, return error
+            res.send(err);
+        }   // if not, return json file
+            res.json(task);
+
+
+    });
+});
+
+
+
+
+
 
 
 // exports for access from other files
